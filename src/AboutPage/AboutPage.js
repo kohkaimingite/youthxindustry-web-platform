f23bc14160a1c9dd1efb284276b33a76d964b12f
andrea <20023855@myrp.edu.sg>
2022-04-20 17:58:28 +0800

    about us page

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -1,17 +1,19 @@
 // JavaScript source code
 import NavBar from './components/NavBar'
 import './App.css';
-
+//<h2>Picure of logo with "about us" button</h2>
+            //<AboutmeSet />
+            //<h2>Picure of logo with "opportunities" button</h2>
+            //<h2>Picure of logo with "contact us" button</h2>
+            //<h2>Picure of logo with "login" button</h2>
 function AboutPage() {
     return (
         <div className="App">
             <NavBar />
-            <h2>Picure of logo with "about us" button</h2>
-            <AboutmeSet />
-            <h2>Picure of logo with "opportunities" button</h2>
-            <h2>Picure of logo with "contact us" button</h2>
-            <h2>Picure of logo with "login" button</h2>
-
+            
+            <Router>
+                <Route path="/Aboutus" component={AboutPage} />
+            </Router>
 
         </div>
     );
