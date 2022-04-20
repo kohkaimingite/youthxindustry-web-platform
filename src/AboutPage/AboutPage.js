b4d85eeb37eb564c0738b26bb7263418f7c30edb
andrea <20023855@myrp.edu.sg>
2022-04-20 17:52:07 +0800

    about us page

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,21 @@
+// JavaScript source code
+import NavBar from './components/NavBar'
+import './App.css';
+
+function AboutPage() {
+    return (
+        <div className="App">
+            <NavBar />
+            <h2>Picure of logo with "about us" button</h2>
+            <AboutmeSet />
+            <h2>Picure of logo with "opportunities" button</h2>
+            <h2>Picure of logo with "contact us" button</h2>
+            <h2>Picure of logo with "login" button</h2>
+
+
+        </div>
+    );
+}
+
+export default AboutPage;
+
