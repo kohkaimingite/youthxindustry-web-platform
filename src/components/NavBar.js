70d6e570464f3adc2d7c2145560b40bcd18fb2e3
Koh Kai Ming <20045346@myrp.edu.sg>
2022-05-09 15:56:24 +0800

    Register function (trying)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,7 +20,7 @@ const NavBar  = () => {
                 
                 <Link to="/ContactUs"><button>Contact us</button> </Link>
                 <Link to="/Login"><button>Login</button> </Link>
-
+                <Link to="/Register"><button>Register</button></Link>
 
             </header>
              
