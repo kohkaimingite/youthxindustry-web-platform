1c8dc55569a53f0793be28afd3c438181182da7c
Loh XQ <20018215@myrp.edu.sg>
2022-05-29 03:04:09 +0800

    hais

--- src/Manage/UserProfile.js
+++ src/Manage/UserProfile.js
@@ -1,6 +1,29 @@
-import React, { Component } from 'react';
 import NavBar from '../components/NavBar'
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "../Some test data/List";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Collapsible from '../components/Collapsible';
+import axios from 'axios';
+
+
 function UserProfile() {
+    const [ProfList, setProfList] = useState([]);
+    const getProfile = () => {
+        axios.get("http://localhost:3001/Profile").then((response) => {
+
+            console.log(response);
+            setProfList(response.data);
+            
+        });
+    };
+    const [Email, setEmail] = useState(ProfList[0]);
+    const [Name, setName] = useState(ProfList[1]);
+    const [Number, setNumber] = useState(ProfList[2]);
+    
+    
+    
     return (
         <div className="App">
             <NavBar />
@@ -12,16 +35,18 @@ function UserProfile() {
                         <text>Full Name:</text><br />
                         <text>Email:</text><br />
                         <text>Mobile Number:</text>
+                        <button type="button" onClick={() => setName(ProfList[0]) && setEmail(ProfList[1]) && setNumber(ProfList[2])}>Load Deatils</button>
                     </form>
-
+                    
                     </div>
                     <div className="AlignMiddle">
                     <form action="/action_page.php" method="post">
                         <h3>Testing</h3>
-                        <text align="Left">Example Lee</text><br />
+                        
+                        <text align="Left">{Name}</text><br />
                         <text align="Left">timmy@xqc.com</text><br />
                         <text align="Left">12345678</text><br />
-                    </form> 
+                    </form>
                     </div>
                 </div>
         </div>
