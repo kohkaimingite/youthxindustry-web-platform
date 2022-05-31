dde7a85a903848fe348301ca7b3dff1068111cc4
Loh XQ <20018215@myrp.edu.sg>
2022-05-31 22:43:34 +0800

    Test

--- src/Manage/EditProfile.js
+++ src/Manage/EditProfile.js
@@ -1,7 +1,37 @@
 // JavaScript source code
-import React, { Component } from 'react';
 import NavBar from '../components/NavBar'
+import { React, useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import List from "../Some test data/List";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Collapsible from '../components/Collapsible';
+import { Route, Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+
 function UserProfile() {
+    const [Check, setCheck] = useState("");
+    const [EmailCheck, setEmailCheck] = useState("");
+    const [Number, setNumber] = useState("");
+    const [Email, setEmail] = useState("");
+    const [NumberCheck, setNumberCheck] = useState("");
+    const [ProfList, setProfList] = useState([]);
+    const columns = ProfList[0] && Object.keys(ProfList[0]);
+    const getProfile = () => {
+
+    };
+    useEffect(() => {
+        axios.get("http://localhost:3001/profile").then((response) => {
+
+            console.log(response);
+            setProfList(response.data);
+
+        });
+    });
+
+
     return (
         <div className="App">
             <NavBar />
@@ -10,31 +40,68 @@ function UserProfile() {
                 <div className="AlignLeft">
                     <h3>Details:</h3>
                     <form action="/action_page.php" method="post">
-                        <label>Name:</label><br />
-                        <text align="Left">timmy@xqc.com</text><br />
-                        <text align="Left">12345678</text><br />
+                        {ProfList.map((val, key) => {
+                            return <text align="Left">Name: {val.name}</text>;
+                        })}<br />
+                        {ProfList.map((val, key) => {
+                            return <text align="Left">Email: {val.email}</text>;
+                        })}<br />
+                        {ProfList.map((val, key) => {
+                            return <text align="Left">Mobile Number: {val.MobileNumber}</text>;
+                        })}<br />
+                        {ProfList.map((val, key) => {
+                            return <text align="Left">Bio: {val.userbio}</text>;
+                        })}<br />
                     </form>
 
                 </div>
                 <div className="AlignMiddle">
-                    <form action="/action_page.php" id="manageform">
-                        
-
-                    </form>
-                    <form action="/action_page.php" method="post">
+                  
+                    <form method="post">
                         <h3>Testing</h3>
-                        <label>New Name:</label>
-                        <input type="text" placeholder="Enter A Name..."></input>
                         <label>New Email:</label>
-                        <textarea name="Email" form="manageform"></textarea>
-                        <label>New Mobile Number:</label><br />
-                        <textarea></textarea>
+                        <input type="text" id='email' placeholder="Enter A Email" onChange={e =>setEmail(e.target.value)}></input><text color='#FF0000'>{EmailCheck}</text><br />
+                        <label>New Mobile Number:</label>
+                        <input type="text" id='number' placeholder="Enter A Mobile Number..." onChange={e => setNumber(e.target.value)}></input><text color='#FF0000'>{NumberCheck}</text><br />
+                        <button onClick={submit}> Confirm </button>
+                        <text align='left'>{Check}</text>
                     </form>
+                    
                 </div>
             </div>
         </div>
+    
     )
-
-
+    //function checkInput() {
+    //  }  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test('email')) {
+    //        setEmailCheck("Ok Email")
+    //    }
+    //    else {
+    //        setEmailCheck("Please Enter a Valid Email!")
+    //    }
+    //    if ('email' == "") {
+    //        setEmailCheck("Please Enter an Email!")
+    //    }
+    //    else if ('email' != "'*'@'*'.com") {
+    //        setEmailCheck("Please Enter A Valid Email")
+    //    }
+    //    else {
+    //        setEmailCheck("Ok Email")
+        
+    //}
+    function submit() {
+        axios.post("http://localhost:3001/EditProfile", {
+            Number: parseInt(Number),
+            Email: Email
+            }).then(() => {
+                console.log("Test");
+                /*setCheck(response.data);*/
+            });
+    };
 }
+
+
+
+
+
 export default UserProfile;
