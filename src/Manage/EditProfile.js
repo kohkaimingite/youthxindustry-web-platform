e26177802a726001d91a68b74a07c71445ab20ee
Loh XQ <20018215@myrp.edu.sg>
2022-06-01 09:35:20 +0800

    gg

--- src/Manage/EditProfile.js
+++ src/Manage/EditProfile.js
@@ -57,11 +57,11 @@ function UserProfile() {
 
                 </div>
                 <div className="AlignMiddle">
-                  
+
                     <form method="post">
                         <h3>Testing</h3>
                         <label>New Email:</label>
-                        <input type="text" id='email' placeholder="Enter A Email" onChange={e =>setEmail(e.target.value)}></input><text color='#FF0000'>{EmailCheck}</text><br />
+                        <input type="text" id='email' placeholder="Enter A Email" onChange={e => setEmail(e.target.value)}></input><text color='#FF0000'>{EmailCheck}</text><br />
                         <label>New Mobile Number:</label>
                         <input type="text" id='number' placeholder="Enter A Mobile Number..." onChange={e => setNumber(e.target.value)}></input><text color='#FF0000'>{NumberCheck}</text><br />
                         <label>New Bio: </label>
@@ -69,44 +69,40 @@ function UserProfile() {
                         <button onClick={submit}> Confirm </button>
                         <text align='left'>{Check}</text>
                     </form>
-                    
+
                 </div>
             </div>
         </div>
-    
+
     )
-    //function checkInput() {
-    //  }  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test('email')) {
-    //        setEmailCheck("Ok Email")
-    //    }
-    //    else {
-    //        setEmailCheck("Please Enter a Valid Email!")
-    //    }
-    //    if ('email' == "") {
-    //        setEmailCheck("Please Enter an Email!")
-    //    }
-    //    else if ('email' != "'*'@'*'.com") {
-    //        setEmailCheck("Please Enter A Valid Email")
-    //    }
-    //    else {
-    //        setEmailCheck("Ok Email")
-        
-    //}
+    function checkInput() {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(Email)) {
+        setEmailCheck("Ok Email")
+    }
+    else {
+        setEmailCheck("Please Enter a Valid Email!")
+    }
+        if ('email' == "") {
+            setEmailCheck("Please Enter an Email!")
+        }
+        else if ('email' != "'*'@'*'.com") {
+            setEmailCheck("Please Enter A Valid Email")
+        }
+        else {
+            setEmailCheck("Ok Email")
+        }
+    }
     function submit() {
         axios.post("http://localhost:3001/EditProfile", {
             Number: parseInt(Number),
             Email: Email,
             Bio: Bio
-            }).then(() => {
-                console.log("Test");
-                /*setCheck(response.data);*/
-                window.location = "http://localhost:3000/Profile";
-            });
+        }).then(() => {
+            console.log("Test");
+            /*setCheck(response.data);*/
+            window.location = "http://localhost:3000/Profile";
+        });
     };
 }//ADD IF STATEMENT TO CHECK FOR IF IT IS SUCCESSFUL
-
-
-
-
 
 export default UserProfile;
