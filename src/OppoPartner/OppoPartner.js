a8202335c8508a15681720deb988496cda507abf
Koh Kai Ming <20045346@myrp.edu.sg>
2022-06-21 22:04:14 +0800

    Connect.js connect middleware

--- src/OppoPartner/OppoPartner.js
+++ src/OppoPartner/OppoPartner.js
@@ -1,13 +1,31 @@
 import NavBar from '../components/NavBar';
-import axios from 'axios';
+import Axios from 'axios';
 import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from 'react-bootstrap/Button';
 
 export default function OppoPartner() {
 
+    Axios.defaults.withCredentials = true;
     const [oppList, setOppList] = useState([]);
+    const columns = oppList[0] && Object.keys(oppList[0]);
+
+
+    const delOppoPartner = (oppId) => {
+        Axios.post("http://localhost:3001/deleteOppPartner", {
+            oppId: oppId
+        }).then(() => {
+            console.log("Deleted sucessfully!");
+        });
+    }
+
+
+
+
     useEffect(() => {
-        axios.get("http://localhost:3001/oppListing").then((response) => {
+        Axios.get("http://localhost:3001/oppListing").then((response) => {
 
             console.log(response);
             setOppList(response.data);
@@ -27,7 +45,7 @@ export default function OppoPartner() {
         display: "flex",
         justifyContent: 'center',
         height: '5vh',
-       
+
     };
 
     return (
@@ -36,14 +54,12 @@ export default function OppoPartner() {
             <div style={styleButton}>
                 <Link to="/AddOppoPartner" className="btn btn-primary" style={{ width: '10%' }}>Add Opportunity</Link>
             </div>
-            <div style={styleButton}>
-                <Link to="/DeleteOppoPartner" className="btn btn-primary" style={{ width: '10%' }}>Delete Opportunity</Link>
-            </div>
             <div style={styleDiv} className="OppoPartner">
-            <table style={{
-                backgroundColor: '#f3f3f3',
-                width: '1300px',
-                height: '300px'} }>
+                <table style={{
+                    backgroundColor: '#f3f3f3',
+                    width: '1300px',
+                    height: '300px',
+                }}>
                     <tr>
                         <th>Job Code</th>
                         <th>Job Name</th>
@@ -52,22 +68,36 @@ export default function OppoPartner() {
                         <th>Address</th>
                         <th>Job Categories</th>
                         <th>Edit/Delete</th>
-                </tr>
-                {oppList.map((val, key) => {
-                    return (
-                        <tr key={key}>
-                            <td>{val.OppID}</td>
-                            <td>{val.Name}</td>
-                            <td>{val.Description}</td>
-                            <td>{val.Location}</td>
-                            <td>{val.Address}</td>
-                            <td>{val.Type}</td>
-                        </tr>
-                    )
-                })}
-            </table>
+
+                    </tr>
+                    {oppList.map(row => <tr>
+                        {
+                            columns.map(column => <td>{row[column]}</td>)
+
+
+                        }
+                        <Button style={{
+
+                            backgroundColor: '#008000',
+                        }}>
+                            <FontAwesomeIcon icon={faPencil} />
+                        </Button>
+                        <Button
+                            style={{
+
+                                backgroundColor: '#B22222',
+                            }}
+                            onClick={() => delOppoPartner(row[columns[0]])}>
+
+                            <FontAwesomeIcon icon={faTrash} />
+                        </Button>
+
+                    </tr>
+
+                    )}
+                </table>
             </div>
-            </div>
+        </div>
     );
 
 }
