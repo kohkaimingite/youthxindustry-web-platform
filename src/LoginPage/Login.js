c1ffac247b681b3de422c635addab01951292a3f
Koh Kai Ming <20045346@myrp.edu.sg>
2022-07-10 23:35:10 +0800

    some css

--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -3,6 +3,8 @@ import Axios from 'axios';
 import NavBar from '../components/NavBar';
 import './Login.css';
 import { useNavigate } from 'react-router-dom';
+import { faSignIn,faUserLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Login() {
 
@@ -59,20 +61,21 @@ export default function Login() {
         <div className="App">
             <NavBar />
             <div className="login">
+                <FontAwesomeIcon icon={faUserLock} />
                 <h1>Login</h1>
 
-                <label>Email</label>
+                <label style={{ marginRight: "150px" }}>Email</label>
                 <input type="email"
-                    placeholder="Email..."
+                    placeholder="Enter email"
                     id="email"
                     required
                     onChange={(e) => {
                         setEmail(e.target.value);
                     }}
                 />
-                <label>Password</label>
+                <label style={{ marginRight: "125px" }}>Password</label>
                 <input type="password"
-                    placeholder="Password..."
+                    placeholder="Enter password"
                     id="password"
                     required
                     onChange={(e) => {
@@ -80,11 +83,10 @@ export default function Login() {
                     }}
                 />
 
-                <button onClick={login}>Login</button>
+                <button onClick={login}>Login <FontAwesomeIcon icon={faSignIn}/></button>
                 <button onClick={logout}>Logout</button>
-
+                <p>{status}</p>
             </div>
-            <h1>{status}</h1>
         </div>
 
     );
