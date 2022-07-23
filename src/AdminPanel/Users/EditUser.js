9d851dfe8b9a867e96e03ac8beb8225642c85f2a
ChooZhiWei <20014956@myrp.edu.sg>
2022-07-23 23:52:30 +0800

    edit user validation before submitting (working)

--- src/AdminPanel/Users/EditUser.js
+++ src/AdminPanel/Users/EditUser.js
@@ -11,6 +11,7 @@ const EditUser = () => {
     const [formErrors, setFormErrors] = useState({});
     const [isSubmit, setIsSubmit] = useState(false);
     const {roleID, name, password, email, age, gender, userBio, contactNumber, userID} = formValues;
+    let { errorCheck } = '';
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -18,31 +19,129 @@ const EditUser = () => {
         console.log(formValues);
     }
     
+    const validate = (values) => {
+        const errors = {};
+        const spRegex = /^\S*$/;
+        const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        const pnumRegex = /(^[689]{1}\d{7}$)/;
+        if (!values.userID) {
+            errorCheck = 1;
+            errors.userID = "UserID is required";
+        } else if (!spRegex.test(values.userID)) {
+            errorCheck = 1;
+            errors.userID = "UserID cannot contain spacing";
+        }
+        if (!values.roleID) {
+            errorCheck = 1;
+            errors.roleID = "RoleID is required";
+        }
+        if (!values.name) {
+            errorCheck = 1;
+            errors.name = "Name is required";
+        } else if (values.name.length > 50) {
+            errorCheck = 1;
+            errors.name = "Name cannot exceed 50 characters";
+        }
+        if (!values.password) {
+            errorCheck = 1;
+            errors.password = "Password is required";
+        } else if (!spRegex.test(values.password)) {
+            errorCheck = 1;
+            errors.password = "Password cannot contain spacing";
+        } else if (!pwRegex.test(values.password)) {
+            errorCheck = 1;
+            errors.password = "Password has to be at least 8 characters with at least 1 letter, and 1 number";
+        } else if (values.password.length > 50) {
+            errorCheck = 1;
+            errors.password = "Password cannot exceed 50 characters";
+        }
+        if (!values.email) {
+            errorCheck = 1;
+            errors.email = "Email is required";
+        } else if (!spRegex.test(values.email)) {
+            errorCheck = 1;
+            errors.email = "Email cannot contain spacing";
+        } else if (!emailRegex.test(values.email)) {
+            errorCheck = 1;
+            errors.email = "Email must be a valid email address";
+        } else if (values.email.length > 50) {
+            errorCheck = 1;
+            errors.email = "Email cannot exceed 50 characters";
+        }
+        if (!values.age) {
+            errorCheck = 1;
+            errors.age = "Age is required";
+        } else if (!spRegex.test(values.age)) {
+            errorCheck = 1;
+            errors.age = "Age cannot contain spacing";
+        } else if (values.age > 100) {
+            errorCheck = 1;
+            errors.age = "Maximum age is 100";
+        }
+        if (!values.gender) {
+            errorCheck = 1;
+            errors.gender = "Gender is required";
+        } else if (values.gender.length > 1) {
+            errorCheck = 1;
+            errors.gender = "Gender only accepts 'M' and 'F' characters";
+        }
+        if (values.userBio.length > 255) {
+            errorCheck = 1;
+            errors.userBio = "Biography cannot exceed 255 characters";
+        }
+        if (!values.contactNumber) {
+            errorCheck = 1;
+            errors.contactNumber = "Contact number is required";
+        } else if (!spRegex.test(values.contactNumber)) {
+            errorCheck = 1;
+            errors.contactNumber = "Contact number cannot contain spacing";
+        } else if (isNaN(values.contactNumber)) {
+            errorCheck = 1;
+            errors.contactNumber = "Enter only in number";
+        } else if (values.contactNumber.length > 8) {
+            errorCheck = 1;
+            errors.contactNumber = "Contact number cannot exceed 8 characters";
+        } else if (!pnumRegex.test(values.contactNumber)) {
+            errorCheck = 1;
+            errors.contactNumber = "Contact number must be a valid number";
+        }
+
+        return errors;
+    }
+
     const submitFormData = (e) => {
         e.preventDefault();
         setFormErrors(validate(formValues));
-        setIsSubmit(true);
-        {
-            axios.post("http://localhost:3001/userEdit", {
-                UserID: userID,
-                RoleID: roleID,
-                name: name,
-                password: password,
-                email: email,
-                age: age,
-                gender: gender,
-                userBio: userBio,
-                contactNumber: contactNumber,
-            })
-                .then((response) => {
-                    console.log(response);
-                    setFormValues({ roleID: "", name: "", password: "", email: "", age: "", gender: "", userBio: "", contactNumber: "", userID: "" })
-                    console.log("Successfully updated");
+        if (errorCheck == 1) {
+            console.log(errorCheck);
+            console.log("Error");
+        } else {
+            console.log(errorCheck);
+            setIsSubmit(true);
+            {
+                axios.post("http://localhost:3001/userEdit", {
+                    UserID: userID,
+                    RoleID: roleID,
+                    name: name,
+                    password: password,
+                    email: email,
+                    age: age,
+                    gender: gender,
+                    userBio: userBio,
+                    contactNumber: contactNumber,
                 })
-                .catch(() => {
-                    console.log("Failed to update");
-                });
+                    .then((response) => {
+                        console.log(response);
+                        setFormValues({ roleID: "", name: "", password: "", email: "", age: "", gender: "", userBio: "", contactNumber: "", userID: "" })
+                        console.log("Successfully updated");
+                    })
+                    .catch(() => {
+                        console.log("Failed to update");
+                    });
+            }
         }
+        
     }
 
     useEffect(() => {
@@ -50,74 +149,7 @@ const EditUser = () => {
         if (Object.keys(formErrors).length === 0 && isSubmit) {
             console.log(formValues);
         }
-    })
-
-    const validate = (values) => {
-        const errors = {}
-        const spRegex = /^\S*$/;
-        const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-        const pnumRegex = /(^[689]{1}\d{7}$)/;
-        if (!values.userID) {
-            errors.userID = "UserID is required";
-        } else if (!spRegex.test(values.userID)) {
-            errors.userID = "UserID cannot contain spacing";
-        }
-        if (!values.roleID) {
-            errors.roleID = "RoleID is required";
-        }
-        if (!values.name) {
-            errors.name = "Name is required";
-        } else if (values.name.length > 50) {
-            errors.name = "Name cannot exceed 50 characters";
-        }
-        if (!values.password) {
-            errors.password = "Password is required";
-        } else if (!spRegex.test(values.password)) {
-            errors.password = "Password cannot contain spacing";
-        } else if (!pwRegex.test(values.password)) {
-            errors.password = "Password has to be at least 8 characters with at least 1 letter, and 1 number";
-        } else if (values.password.length > 50) {
-            errors.password = "Password cannot exceed 50 characters";
-        }
-        if (!values.email) {
-            errors.email = "Email is required";
-        } else if (!spRegex.test(values.email)) {
-            errors.email = "Email cannot contain spacing";
-        } else if (!emailRegex.test(values.email)) {
-            errors.email = "Email must be a valid email address";
-        } else if (values.email.length > 50) {
-            errors.email = "Email cannot exceed 50 characters";
-        }
-        if (!values.age) {
-            errors.age = "Age is required";
-        } else if (!spRegex.test(values.age)) {
-            errors.age = "Age cannot contain spacing";
-        } else if (values.age > 100) {
-            errors.age = "Maximum age is 100";
-        }
-        if (!values.gender) {
-            errors.gender = "Gender is required";
-        } else if (values.gender.length > 1) {
-            errors.gender = "Gender only accepts 'M' and 'F' characters";
-        }
-        if (values.userBio.length > 255) {
-            errors.userBio = "Biography cannot exceed 255 characters";
-        }
-        if (!values.contactNumber) {
-            errors.contactNumber = "Contact number is required";
-        } else if (!spRegex.test(values.contactNumber)) {
-            errors.contactNumber = "Contact number cannot contain spacing";
-        } else if (isNaN(values.contactNumber)) {
-            errors.contactNumber = "Enter only in number";
-        } else if (values.contactNumber.length > 8) {
-            errors.contactNumber = "Contact number cannot exceed 8 characters";
-        } else if (!pnumRegex.test(values.contactNumber)) {
-            errors.contactNumber = "Contact number must be a valid number";
-        }
-
-        return errors;
-    }
+    }, [formErrors])
 
     return (
         <div className="App">
