762d993d1a7048b1b5960c1498fb028e6a5e0cec
Loh XQ <20018215@myrp.edu.sg>
2022-08-02 06:05:47 +0800

    g

--- src/Manage/SubmitApplication.js
+++ src/Manage/SubmitApplication.js
@@ -36,53 +36,56 @@ function SubmitApplication() {
     return (
         <div className="App">
             <LoggedNavBar />
-            <div className="main">
-                <h1>Application</h1>
-                <div className="AlignLeft">
-                    <h3>Details:</h3>
-                    <form action="/action_page.php" method="post">
-                        <text>Full Name:</text><br />
-                        <text>Email:</text><br />
-                        <text>Mobile Number:</text><br />
-                    </form>
-                </div>
-            
-                <div className="AlignMiddle">
-                    <h3>Please Check Your Information</h3>
-                    <form action="/action_page.php" method="post">
-                        {ProfList.map((val, key) => {
-                            return <text align="Left">{val.Name}</text>;
-                        })}<br />
-                        {ProfList.map((val, key) => {
-                            return <text align="Left">{val.Email}</text>;
-                        })}<br />
-                        {ProfList.map((val, key) => {
-                            return <text align="Left">{val.ContactNumber}</text>;
-                        })}<br /><br />
-                            
-                        <button onClick={submit}> Confirm </button>
-                        <h3>Ensure that your have uploaded your Resume in your profile page</h3>
-                    </form>
-                </div>
-                <div className="AlignRight">
-                    <Row>
-                        {OppList.map((opp, k) => (
+            <div classname="leftSide">
+                <titleSection2>
+                    <h1> Details </h1>
+                </titleSection2>
+                <leftSection style={{ textAlign: "left" }}>
+                    <text style={{ fontSize: "20px" }}>Name: </text><br />
+                    {ProfList.map((val, key) => {
+                        return <text style={{ fontSize: "20px" }}>{val.Name} </text>;
+                    })}
+                </leftSection>
+                <leftSection style={{ textAlign: "left" }}>
+                    <text style={{ fontSize: "20px" }}>Contact Number: </text><br />
+                    {ProfList.map((val, key) => {
+                        return <text style={{ fontSize: "20px" }}>{val.ContactNumber} </text>;
+                    })}
+                </leftSection>
+                <leftSection style={{ textAlign: "left" }}>
+                    <text style={{ fontSize: "20px" }}>Email: </text><br />
+                    {ProfList.map((val, key) => {
+                        return <text style={{ fontSize: "20px" }}>{val.Email} </text>;
+                    })}
+                </leftSection>
+                <leftSection2>
+                    <text style={{ fontSize: "20px" }}>Please upload your resume at your profile if you have yet to do so</text>
+                </leftSection2>
+                <leftSection2>
+                    <h3>Please ensure that all Information is accurate before submitting</h3>
+                </leftSection2>
+                <button type="primary" onClick={submit}>Submit Application</button>
+            </div>
 
-                                <Card border="dark" style={{ float: 'right' }}>
-                                    <Card.Header style={{ textAlign: 'left' }}>{opp.OppID}</Card.Header>
-                                    <Card.Body>
-                                        <Card.Title style={{ textAlign: 'left' }}><a href={"/Oppo/" + opp.OppID}>{opp.Name}</a></Card.Title>
-                                        <Card.Subtitle style={{ textAlign: 'left', color: 'grey' }} >{opp.Type}</Card.Subtitle>
-                                        <Card.Text style={{ textAlign: 'left', fontSize: "16px" }} ><FontAwesomeIcon icon={faGraduationCap} fontSize="11px" /> {opp.Qualification}</Card.Text>
-                                        <Card.Text style={{ textAlign: 'left', fontSize: "16px" }} > <FontAwesomeIcon icon={faMapMarkerAlt} /> {opp.Location}</Card.Text>
-                                        <Card.Text style={{ textAlign: 'left', fontSize: "16px" }} > <FontAwesomeIcon icon={faDollarSign} /> {opp.Pay}</Card.Text>
-                                    </Card.Body>
-                                </Card>
 
-                        ))}
-                    </Row>
-                            
-                </div>
+            <div className="AlignRight2">
+                <Row>
+                    {OppList.map((opp, k) => (
+
+                        <Card border="dark" style={{ float: 'right' }}>
+                            <Card.Header style={{ textAlign: 'left' }}>{opp.OppID}</Card.Header>
+                            <Card.Body>
+                                <Card.Title style={{ textAlign: 'left' }}><a href={"/Oppo/" + opp.OppID}>{opp.Name}</a></Card.Title>
+                                <Card.Subtitle style={{ textAlign: 'left', color: 'grey' }} >{opp.Type}</Card.Subtitle>
+                                <Card.Text style={{ textAlign: 'left', fontSize: "16px" }} ><FontAwesomeIcon icon={faGraduationCap} fontSize="11px" /> {opp.Qualification}</Card.Text>
+                                <Card.Text style={{ textAlign: 'left', fontSize: "16px" }} > <FontAwesomeIcon icon={faMapMarkerAlt} /> {opp.Location}</Card.Text>
+                                <Card.Text style={{ textAlign: 'left', fontSize: "16px" }} > <FontAwesomeIcon icon={faDollarSign} /> {opp.Pay}</Card.Text>
+                            </Card.Body>
+                        </Card>
+
+                    ))}
+                </Row>
+
             </div>
         </div>
         
